c06cf5c2b2b9e4f73214298aa6d2a312a1a7a8a4
Pan Liu <us.panliu@gmail.com>
2022-07-27 14:02:52 -0700

    add schemas for PostgreSQL and MongoDB

---
+++ databases/mongoSchema.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const productSchema = mongoose.Schema({
+  product_id: Number,
+  name: String,
+  slogan: String,
+  description: String,
+  category: String,
+  default_price: String,
+  features: [
+    {
+      feature: String,
+      value: String,
+    }
+  ],
+  styles: [
+    {
+      style_id: Number,
+      name: String,
+      original_price: String,
+      sale_price: String,
+      default_style: Boolean,
+      photos: [
+        {
+          thumbnail_url: String,
+          url: String
+        }
+      ],
+      skus: [
+        {
+          size: String,
+          quantity: Number,
+        }
+      ]
+    }
+  ],
+  related: [
+    {
+      related_id: Number,
+    }
+  ],
+});
+
+
+
+
--- databases/postgreSchema.sql
+++ databases/postgreSchema.sql
@@ -1,3 +1,67 @@
 DROP DATABASE IF EXISTS products;
 
 CREATE DATABASE products;
+
+\c products;
+
+DROP TABLE IF EXISTS products, features, styles, related, photos, skus;
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  slogan VARCHAR(255) NOT NULL,
+  description VARCHAR(1000) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  default_price VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE features (
+  id SERIAL PRIMARY KEY,
+  feature VARCHAR(100) NOT NULL,
+  value VARCHAR(255) NOT NULL,
+  product_id INT NOT NULL,
+  CONSTRAINT fk_product_feature
+    FOREIGN KEY(product_id)
+      REFERENCES products(id)
+);
+
+CREATE TABLE styles (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  original_price VARCHAR(50) NOT NULL,
+  sale_price VARCHAR(50) NOT NULL,
+  default_style BOOLEAN NOT NULL,
+  product_id INT NOT NULL,
+  CONSTRAINT fk_product_style
+    FOREIGN KEY(product_id)
+      REFERENCES products(id)
+);
+
+CREATE TABLE related (
+  id SERIAL PRIMARY KEY,
+  related_id INT NOT NULL,
+  current_id INT NOT NULL,
+  CONSTRAINT fk_product_related
+    FOREIGN KEY(current_id)
+      REFERENCES products(id)
+);
+
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  url TEXT NOT NULL,
+  thumbnail_url TEXT NOT NULL,
+  style_id INT NOT NULL,
+  CONSTRAINT fk_style_photo
+   FOREIGN KEY(style_id)
+     REFERENCES styles(id)
+);
+
+CREATE TABLE skus (
+  id SERIAL PRIMARY KEY,
+  quantity INT NOT NULL,
+  size VARCHAR(20) NOT NULL,
+  style_id INT NOT NULL,
+  CONSTRAINT fk_style_sku
+    FOREIGN KEY(style_id)
+      REFERENCES styles(id)
+);
